73904ba2b295d3255928869b2938a2e7531209fb
Rob Rau <Rob Rau>
2017-04-08 20:20:36 +0100

    Added latest piflysetup script.

---
+++ piflysetup.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+# This script takes a fresh Raspberry Pi Zero NOOBS 2.3.0 install (No other installations!) and sets up the PiFly development environment
+#
+#Written: 3/26/2017
+#  Rev.: 1.00
+#   By: Robert S. Rau & Rob F. Rau II
+#
+#Updated: 3/26/2017
+#  Rev.: 1.01
+#   By: Robert S. Rau & Rob F. Rau II
+# Changes: fixed matplotlib, creating pifly/log, pkt2wave
+#
+#Updated: 3/26/2017
+#  Rev.: 1.02
+#   By: Robert S. Rau & Rob F. Rau II
+# Changes: added links for GPIO support, audio output, shutdown support, I2C, SPI. setup. Updated gcc command line for nbfm. cd to working directory in #1 (pifly)
+#
+#Updated: 4/2/2017
+#  Rev.: 1.03
+#   By: Robert S. Rau & Rob F. Rau II
+# Changes: added sudo check, added cmdline.txt, added apt-get update
+#
+#Updated: 4/2/2017
+#  Rev.: 1.04
+#   By: Robert S. Rau & Rob F. Rau II
+# Changes: added logging
+#
+#Updated: 4/4/2017
+#  Rev.: 1.05
+#   By: Robert S. Rau & Rob F. Rau II
+# Changes: fixed file append for log file, added linux version, Pi version, and date/time to log, comments about time setup, changed log file name to all lower case, Adafruit_GPIO_Hale
+#
+#Updated: 4/5/2017
+#  Rev.: 1.06
+#   By: Robert S. Rau & Rob F. Rau II
+# Changes: fixed file append for log file after scrot and matplorlib
+#
+#
+#
+#Time setup
+# see http://raspberrypi.stackexchange.com/questions/47542/raspberry-pi-wont-update-time
+#sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+#
+logFilePath=/var/log/piflyinstalllog.txt
+#
+# 0) Check that we are running with root permissions
+if [[ $EUID > 0 ]]; then
+	echo "Please run using sudo"
+	exit
+fi
+echo "" >> $logFilePath
+echo "PiFly Setup Script" >> $logFilePath
+date >> $logFilePath
+uname -a >> $logFilePath
+cat /proc/cpuinfo >> $logFilePath
+echo "PiFly Setup:Start Run in sudo" >> $logFilePath
+#
+#
+#
+#
+#
+#
+# 1) Setup directory structure
+echo "PiFly setup: Starting directory setup"
+cd ~
+mkdir pifly
+echo "PiFly Setup:mkdir pifly:result" $? >> $logFilePath
+# switch to install directory
+cd ~/pifly
+#
+#
+#
+sudo apt-get update
+echo "PiFly Setup:mkdir pifly:sudo apt-get update" $? >> $logFilePath
+#
+#
+# 2) Set up Raspberry Pi configuration
+# see:https://www.raspberrypi.org/forums/viewtopic.php?f=44&t=130619
+# for SPI see (see DMA note at bottom):https://www.raspberrypi.org/documentation/hardware/raspberrypi/spi/README.md
+echo "PiFly setup: StartingRaspberry Pi configuration"
+#
+sudo apt-get install git
+#
+# Make USB drive writeable
+#I don't know how to do this
+#
+#
+# Make TV output secondary to HDMI output (I have read this is the default, but not so in practice)
+#I don't know how to do this. I have made one that ONLY uses TV output..useless!
+#
+#
+# shutdown support
+# http://www.recantha.co.uk/blog/?p=13999
+#To run automatically at startup, move the executable to /usr/local/bin and
+#edit /etc/rc.local, inserting this one line before the final 'exit 0':
+#/usr/local/bin/gpio-halt &
+#An alternate pin number can optionally be specified before the '&'
+#
+git clone https://github.com/adafruit/Adafruit-GPIO-Halt
+echo "PiFly Setup:git clone of Adafruit_GPIO_Halt" $? >> $logFilePath
+cd Adafruit-GPIO-Halt
+make
+echo "PiFly Setup:make of Adafruit_GPIO_Halt" $? >> $logFilePath
+sudo make install
+echo "PiFly Setup:make install of Adafruit_GPIO_Halt" $? >> $logFilePath
+#
+#
+# Enable SPI, I2c, I2S.
+# see  http://raspberrypi.stackexchange.com/questions/14229/how-can-i-enable-the-camera-without-using-raspi-config
+#
+#
+# Edit cmdline.txt st serial port is available for GPS
+sudo sed -i.bak -e 's/console=ttyAMA0\,115200 //' -e 's/kgdboc=ttyAMA0,115200 //' -e 's/console=serial0,115200 //' /boot/cmdline.txt
+echo "PiFly Setup:mkdir pifly:cmdline.txt update" $? >> $logFilePath
+#
+# 3) install RF transmitters and modulators
+#
+#  nbfm - narrow band FM - 144MHz transmitter, uses GPIO4
+wget https://raw.githubusercontent.com/fotografAle/NBFM/master/nbfm.c
+gcc -o3 -lm -std=gnu99 -o nbfm nbfm.c           # changed from -std=c99 to -std=gnu99
+echo "PiFly Setup:mkdir pifly:gcc nbfm" $? >> $logFilePath
+#
+#
+#  rpitx - able to TX on 440MHz band, uses GPIO18
+echo "PiFly setup: Startingrpitx setup"
+cd ~/pifly
+git clone https://github.com/F5OEO/rpitx
+echo "PiFly Setup:mkdir pifly:git clone of rpitx result" $? >> $logFilePath
+cd ./rpitx
+sudo ./install.sh
+echo "PiFly Setup:mkdir pifly:rpitx install result" $? >> $logFilePath
+#
+#
+#  pifm - able to TX on 144MHz band, uses GPIO4
+# It's all Pythony, I don't know how to do this
+echo "PiFly setup: Startingpifmsetup"
+#sudo python
+#>>> import PiFm
+#
+#
+#  Packet radio modulator. Text to .WAV file
+wget https://raw.githubusercontent.com/km4efp/pifox/master/pifox/pkt2wave
+chmod +x pkt2wave
+echo "PiFly Setup:mkdir pifly:wget pkt2wave result" $? >> $logFilePath
+#
+#
+#
+#  slow scan TV
+# will add later
+#https://www.element14.com/community/community/raspberry-pi/raspberrypi_projects/blog/2014/01/27/pi-noir-and-catch-santa-challenge--the-dutch-way
+#
+#
+# 4) install audio support
+#
+# Text to speech    see:https://learn.adafruit.com/speech-synthesis-on-the-raspberry-pi/installing-the-festival-speech-package
+echo "PiFly setup: Startingfestivalsetup"
+sudo apt-get -y install festival
+echo "PiFly Setup:mkdir pifly:apt-get festival result" $? >> $logFilePath
+#
+#
+# set up audio output
+#  https://learn.adafruit.com/adding-basic-audio-ouput-to-raspberry-pi-zero/pi-zero-pwm-audio
+#
+#
+# 5) Video download link support
+#
+# Python matplotlib
+sudo apt-get -y install python-matplotlib
+#
+echo "PiFly Setup:mkdir pifly:apt-getpython-matplotlibresult" $? >> $logFilePath
+#
+#
+# 6) High current output support
+#
+# see  http://abyz.co.uk/rpi/pigpio/index.html
+#
+#
+#
+# 7) Developer tools
+#
+# Screen capture tool
+sudo apt-get -y install scrot
+echo "PiFly Setup:mkdir pifly:apt-getapt-get scrotresult" $? >> $logFilePath
+#
+#
+# Should load some libpifly examples
