645db5045b2f71100fc17d062ad84e232011b00d
Robert S. Rau <robertrau@mac.com>
2017-08-06 18:41:44 -0400

    Added more test scripts

--- SPItest.sh
+++ SPItest.sh
@@ -6,12 +6,12 @@
 #    Rev.: 1.00
 #      By: Robert S. Rau
 #
+# Updated: 5/10/2017
+#    Rev.: 1.01
+#      By: Robert S. Rau
+# Changes: removed sudo requirement, added channels, fixed missing $
 #
-#  Check that we are running with root permissions
-if [[ $EUID > 0 ]]; then
-	echo "Please run using: sudo "
-	exit
-fi
+#
 #
 #
 # GPIO assignments
@@ -27,149 +27,18 @@ SPIADS7957=8
 #
 gpio -g mode $SPIADS7957 out
 gpio -g write $SPIADS7957 1
+gpio -g mode $SPIMMA6555 out
+gpio -g write $SPIMMA6555 1
 gpio -g mode $SPICLK out
 gpio -g write $SPICLK 0
 gpio -g mode $SPIMOSI out
 gpio -g write $SPIMOSI 0
 gpio -g mode $SPIMISO in
-gpio -g mode $SPIMMA6555 out
-gpio -g write $SPIMMA6555 1
 #
 #
 #
-# assert CS
-gpio -g write SPIADS7957 0
-#
-#
-# Mode control register settings
-ADRWORD="0"      #A/D Read WORD
-CGRWORD="0"      #Channel or Gpio Read WORD
-# SPI loop
-#for (( bitCount = 1; bitCount <= 16; bitCount++ ))
-#do
-gpio -g write $SPIMOSI 0   #Manual mode
-gpio -g write $SPICLK 1
-CGRWORD=$(($CGRWORD+$CGRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-CGRWORD=$(($CGRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0   #Manual mode
-gpio -g write $SPICLK 1
-CGRWORD=$(($CGRWORD+$CGRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-CGRWORD=$(($CGRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0   #Manual mode
-gpio -g write $SPICLK 1
-CGRWORD=$(($CGRWORD+$CGRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-CGRWORD=$(($CGRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 1   #Manual mode
-gpio -g write $SPICLK 1
-CGRWORD=$(($CGRWORD+$CGRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-CGRWORD=$(($CGRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 1   # enable prog bits 6-0
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0    # channel 0000
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0    # channel 0000
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0    # channel 0000
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0    # channel 0000
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 1     # 5v ref
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0   # no power down
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 0   # 4 MSBs are channel addr
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 1
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 1
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 1
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-gpio -g write $SPIMOSI 1
-gpio -g write $SPICLK 1
-ADRWORD=$(($ADRWORD+$ADRWORD))
-ADBIT="$(gpio -g read $SPIMISO)"
-ADRWORD=$(($ADRWORD+$ADBIT))
-gpio -g write $SPICLK 0
-#
-#done
-gpio -g write $SPIADS7957 1
-echo $CGRWORD "  " $ADRWORD
-#
-#
-#
-#
-#
-#
-# assert CS
-gpio -g write SPIADS7957 0
+# assert CS    channel 0  (J7 pin 3), read out junk
+gpio -g write $SPIADS7957 0
 #
 #
 # Mode control register settings
@@ -299,12 +168,403 @@ echo $CGRWORD "  " $ADRWORD
 #
 #
 #
+# assert CS     mode: channel 6, temperature    read out channel 0
+gpio -g write $SPIADS7957 0
+#
+#
+# Mode control register settings
+ADRWORD="0"      #A/D Read WORD
+CGRWORD="0"      #Channel or Gpio Read WORD
+# SPI loop
+#for (( bitCount = 1; bitCount <= 16; bitCount++ ))
+#do
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1   # enable prog bits 6-0
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0    # channel 0110
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 0110
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 0110
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0    # channel 0110
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1     # 5v ref
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   # no power down
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   # 4 MSBs are channel addr
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+#done
+gpio -g write $SPIADS7957 1
+echo $CGRWORD "  " $ADRWORD
 #
 #
 #
 #
-# assert CS
-gpio -g write SPIADS7957 0
+#
+#
+#
+#
+#
+#
+# assert CS     mode: channel 7, keypad    read out channel 6
+gpio -g write $SPIADS7957 0
+#
+#
+# Mode control register settings
+ADRWORD="0"      #A/D Read WORD
+CGRWORD="0"      #Channel or Gpio Read WORD
+# SPI loop
+#for (( bitCount = 1; bitCount <= 16; bitCount++ ))
+#do
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1   # enable prog bits 6-0
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0    # channel 0111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 0111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 0111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 0111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1     # 5v ref
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   # no power down
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   # 4 MSBs are channel addr
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+#done
+gpio -g write $SPIADS7957 1
+echo $CGRWORD "  " $ADRWORD
+#
+#
+#
+#
+#
+# assert CS     mode: channel 15, fire A output voltage    read out channel 7
+gpio -g write $SPIADS7957 0
+#
+#
+# Mode control register settings
+ADRWORD="0"      #A/D Read WORD
+CGRWORD="0"      #Channel or Gpio Read WORD
+# SPI loop
+#for (( bitCount = 1; bitCount <= 16; bitCount++ ))
+#do
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1   #Manual mode
+gpio -g write $SPICLK 1
+CGRWORD=$(($CGRWORD+$CGRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+CGRWORD=$(($CGRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1   # enable prog bits 6-0
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 1111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 1111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 1111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1    # channel 1111
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1     # 5v ref
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   # no power down
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 0   # 4 MSBs are channel addr
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+gpio -g write $SPIMOSI 1
+gpio -g write $SPICLK 1
+ADRWORD=$(($ADRWORD+$ADRWORD))
+ADBIT="$(gpio -g read $SPIMISO)"
+ADRWORD=$(($ADRWORD+$ADBIT))
+gpio -g write $SPICLK 0
+#
+#done
+gpio -g write $SPIADS7957 1
+echo $CGRWORD "  " $ADRWORD
+#
+#
+#
+#
+#
+# assert CS     mode: channel 0    read out channel 15
+gpio -g write $SPIADS7957 0
 #
 #
 # Mode control register settings
